39481bb03926d025b3d428e0df8a5bd14e050ba6
Craig Thacker <craigthackerx@gmail.com>
2022-05-15 19:58:40 +0100

    Update

--- network-profile.tf
+++ network-profile.tf
@@ -1,5 +1,5 @@
 resource "azurerm_network_profile" "net_prof" {
-  count               = var.vnet_integration_enabled && var.os_type == "Linux" ? [1] : []
+  count               = var.vnet_integration_enabled && var.os_type == "Linux" ? 1 : 0
   location            = var.location
   name                = var.network_profile_name
   resource_group_name = var.rg_name
